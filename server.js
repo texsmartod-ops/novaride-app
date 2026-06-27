01d30ccc4005500978364da64bbd7145ef271b71
user <user@MacBook-Air-mac.local>
2026-06-27 10:53:23 +0300

    Add PWA app shell metadata

--- server.js
+++ server.js
@@ -42,6 +42,8 @@ const mimeTypes = {
   ".html": "text/html; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
   ".svg": "image/svg+xml",
   ".png": "image/png",
   ".jpg": "image/jpeg",
