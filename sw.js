25b13d70f5bde2511506d1ab551eb621c8f38727
user <user@MacBook-Air-mac.local>
2026-06-30 12:06:13 +0300

    Move locate button to map corner

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "novaride-shell-v34";
+const CACHE_NAME = "novaride-shell-v35";
 const SHELL_ASSETS = [
   "/",
   "/index.html",
-  "/styles.css?v=20260630-005",
+  "/styles.css?v=20260630-006",
   "/app.js?v=20260630-002",
   "/assets/odessa-streets.json",
   "/manifest.webmanifest",
